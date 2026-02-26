289454e58e42c0a650cd34d81efc6dd9946f3eef
uosry23 <uosryahmed23@gmail.com>
2026-02-27 01:05:32 +0200

    env

--- web/lib/firebaseConfig.ts
+++ web/lib/firebaseConfig.ts
@@ -3,18 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAcFEMaEyEc1aeebzkpqy6R2It9-4fA9ZE",
-  authDomain: "cs303-3d9c5.firebaseapp.com",
-  projectId: "cs303-3d9c5",
-  storageBucket: "cs303-3d9c5.firebasestorage.app",
-  messagingSenderId: "52010993972",
-  appId: "1:52010993972:web:b412c399a8a08644317d61",
-  measurementId: "G-K6XFRNMWN9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
