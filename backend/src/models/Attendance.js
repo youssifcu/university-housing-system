50b1fe2288e119425e7e3891fa932e077d15eddb
youssifcu <202327124@std.sci.cu.edu.eg>
2026-04-11 15:40:35 +0200

    Update Attendance.js

--- backend/src/models/Attendance.js
+++ backend/src/models/Attendance.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const AttendanceSchema = new mongoose.Schema({
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   recordedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  
+
   attendanceType: {
     type: String,
     enum: ['morning', 'evening', 'meal'],
     required: true
   },
-  
+
   scannedQRCode: { type: String },
   status: { type: String, enum: ['present', 'absent', 'excused'], default: 'present' },
-  
+
   timestamp: { type: Date, default: Date.now },
   date: { type: Date, required: true },
-  
+
   notes: { type: String },
   wasOnLeave: { type: Boolean, default: false }
 });
